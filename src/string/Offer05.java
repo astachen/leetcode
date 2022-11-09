6b1d40b0518d636939902ecb9a7a2765aaa333a6
astachen <xingcanchen2401@163.com>
2022-11-09 23:10:09 +0800

    Complete leetcode Offer05.

---
+++ src/string/Offer05.java
@@ -0,0 +1,20 @@
+package string;
+
+// A pretty simple question: replace the blank spaces with %20
+public class Offer05 {
+    public static String replaceSpace(String src) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : src.toCharArray())
+        {
+            if(c == ' ') res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String src = "We are happy.";
+        String fnl = Offer05.replaceSpace(src);
+        System.out.println(fnl);
+    }
+}
